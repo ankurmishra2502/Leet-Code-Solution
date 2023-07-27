7d0a9f6063c5a393ff73ef58355cdfd8a321954d
ankurmishra2502 <77336311+ankurmishra2502@users.noreply.github.com>
2023-07-27 22:15:48 +0530

    Time: 0 ms (100%), Space: 5.9 MB (78.73%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minSteps(int n) {
+		int ans = 0, i = 2;
+        while(n!=1){
+            if(n%i == 0){
+                n = n/i;
+                ans += i;
+            }
+            else
+                i++;
+        }
+        
+        return ans;
+    }
+};
