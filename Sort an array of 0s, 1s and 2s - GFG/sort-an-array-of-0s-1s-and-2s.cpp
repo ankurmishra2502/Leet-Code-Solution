9953bc4effc7e75813eeec2e77b5477f6271de3f
Ankur Mishra <77336311+ankurmishra2502@users.noreply.github.com>
2023-12-14 17:23:42 +0530

    Added solution - LeetHub

---
+++ Sort an array of 0s, 1s and 2s - GFG/sort-an-array-of-0s-1s-and-2s.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    void sort012(int a[], int n)
+    {
+        map<int,int> mp;
+        for(int i=0;i<n;i++){
+            mp[a[i]]++;
+        }
+        int index=0;
+        for(auto x:mp){
+            int val= x.first;
+            for(int i=0;i<x.second;i++){
+                a[index]=val;
+                index++;
+            }
+        }
+    }
+    
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >>n;
+        int a[n];
+        for(int i=0;i<n;i++){
+            cin >> a[i];
+        }
+
+        Solution ob;
+        ob.sort012(a, n);
+
+        for(int i=0;i<n;i++){
+            cout << a[i]  << " ";
+        }
+
+        cout << endl;
+        
+        
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
